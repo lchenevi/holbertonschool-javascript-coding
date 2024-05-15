57899e6f4caf30b90d80a89710bdb05f2b23312a
lchenevi <leo.cheneviere@gmail.com>
2024-05-15 19:32:59 +0200

    commiting some changes

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-
+/* script that reads and prints the content of a file. */
 const fs = require('fs');
 
+// Read the file specified in the command line arguments using fs.readFile
+// The file path is provided as the third argument (index 2) in the argv array
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  // Check if there's an error while reading the file
   if (err) console.log(err); else console.log(data);
 });
