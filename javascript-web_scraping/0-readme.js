6fd1b3b5f5468fca4d473574220a619e4feccae5
lchenevi <leo.cheneviere@gmail.com>
2024-05-15 19:27:51 +0200

    commiting some changes

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,11 +2,11 @@
 /* print in console whats inside an error file, if this file cannot be read */
 
 // Import the 'fs' module to work with files
-const fs = require('fs');
+import { readFile } from 'fs';
 // Get the file path from command line arguments
 const cheminDuFichier = process.argv[2];
 // Read the content of the file using 'fs.readFile'
-fs.readFile(cheminDuFichier, 'utf8', callbackLectureFichier);
+readFile(cheminDuFichier, 'utf8', callbackLectureFichier);
 
 // Define the callback function for reading the file
 function callbackLectureFichier (erreur, contenu) {
